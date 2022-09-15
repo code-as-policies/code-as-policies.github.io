c321aeea5cf9baf98896d54b7fb74c2f8ee0bca7
Jacky Liang <jackyliang@cmu.edu>
2022-09-15 10:47:18 -0700

    video pausing

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
         vid.play();
     }
 
-    var vid_times = {
+    var vid_start_times = {
         "blocks": {
             1: 0 * 60 + 3,
             2: 1 * 60 + 34,
@@ -71,7 +71,73 @@ $(document).ready(function() {
             11: 7 * 60 + 53,
         }
     }
-     
+
+    var vid_end_times = {
+        "blocks": {
+            1: 1 * 60 + 32,
+            2: 2 * 60 + 17,
+            3: 2 * 60 + 50,
+            4: 3 * 60 + 3,
+            5: 4 * 60 + 51,
+            6: 6 * 60 + 10,
+            7: 7 * 60 + 29,
+            8: 8 * 60 + 53,
+            9: 9 * 60 + 5,
+            10: 9 * 60 + 41,
+            11: 11 * 60 + 15,
+            12: 12 * 60 + 47
+        },
+        "blocksbowls": {
+            1: 0 * 60 + 48,
+            2: 1 * 60 + 25,
+            3: 1 * 60 + 49,
+            4: 2 * 60 + 57,
+            5: 4 * 60 + 16,
+            6: 4 * 60 + 29,
+            7: 4 * 60 + 41,
+            8: 5 * 60 + 42,
+            9: 6 * 60 + 11
+        },
+        "fbp": {
+            1: 0 * 60 + 22,
+            2: 0 * 60 + 43,
+            3: 1 * 60 + 32,
+            4: 3 * 60 + 27,
+            5: 4 * 60 + 10,
+            6: 4 * 60 + 53,
+            7: 5 * 60 + 32,
+        },
+        "draw": {
+            1: 0 * 60 + 38,
+            2: 1 * 60 + 3,
+            3: 2 * 60 + 22,
+            4: 3 * 60 + 5,
+            5: 3 * 60 + 43,
+            6: 4 * 60 + 15,
+            7: 4 * 60 + 59,
+            8: 5 * 60 + 31,
+            9: 6 * 60 + 21,
+            10: 7 * 60 + 52,
+            11: 8 * 60 + 33,
+        }
+    }
+
+    var vid_should_check_pause = {
+        'blocks': false,
+        'blocksbowls': false,
+        'fbp': false,
+        'draw': false
+    };
+
+    Object.keys(vid_should_check_pause).forEach(domain_name => {
+        const events = ["seeking", "ended"];
+        events.forEach(event => {
+            $("#vid_" + domain_name).addEventListener(event, function() {
+                vid_should_check_pause[domain_name] = false;
+            });
+        })
+    });
+
     // demos
     $('select').on('change', function() {
         var sep_idx = this.value.indexOf('_');
@@ -89,10 +155,22 @@ $(document).ready(function() {
                 $('#vid_1_' + domain_name + "_" + current_cmd_idx.toString()).get(0).pause();
                 // $('#vid_2_' + domain_name + "_" + current_cmd_idx.toString()).get(0).pause();
             } else {
-                // set vido timestamp
+                // set vid timestamp
                 var vid = $("#vid_" + domain_name)[0];
-                var time = vid_times[domain_name][desired_cmd_idx];
+                var time = vid_start_times[domain_name][desired_cmd_idx];
                 setTime(vid, time);
+
+                // pause video after current segment finishes 
+                vid_should_check_pause[domain_name] = true;
+                var pausing_function = function() {
+                    if (vid_should_check_pause[domain_name] && 
+                        this.currentTime >= vid_end_times[domain_name][desired_cmd_idx]) {
+                        
+                        this.pause();
+                        this.removeEventListener("timeupdate", pausing_function);
+                    }
+                };
+                vid.addEventListener("timeupdate", pausing_function);
             }
 
             // show desired content
